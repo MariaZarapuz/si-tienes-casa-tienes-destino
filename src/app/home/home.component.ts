c2185cdf6aecd91679b810988c31619adcd8254e
Amartinlopez <andre.martin.lopez@gmail.com>
2020-02-29 22:24:52 +0100

    hero slider

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  slide: string;
+
+  constructor() {
+    this.slide = 'slide1';
+  }
 
   ngOnInit() {
+    setInterval(() => {
+      if (this.slide === 'slide1') {
+        this.slide = 'slide2'
+      } else if (this.slide === 'slide2') {
+        this.slide = 'slide3'
+      } else if (this.slide === 'slide3') {
+        this.slide = 'slide1'
+      }
+    }, 8000)
   }
 
 }
