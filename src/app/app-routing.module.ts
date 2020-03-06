73bffc089c5f8c5025766d188c0fe83e8b73f43c
Amartinlopez <andre.martin.lopez@gmail.com>
2020-03-06 12:57:45 +0100

    Mucho avance en maquetación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'user', component: UserComponent },
+  { path: 'house-detail', component: HouseDetailComponent },
   { path: '**', redirectTo: 'home' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { HouseDetailComponent } from './house-detail/house-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent
+    UserComponent,
+    HouseDetailComponent,
   ],
   imports: [
     BrowserModule,
