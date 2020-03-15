f91ec840eb589e1c0b4e501d8322b81af48cf978
MariaZarapuz <mariazarapuz@gmail.com>
2020-03-15 23:27:15 +0100

    retoques en search y detai-house

--- src/app/house-detail/house-detail.component.ts
+++ src/app/house-detail/house-detail.component.ts
@@ -1,34 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-house-detail',
-  templateUrl: './house-detail.component.html',
-  styleUrls: ['./house-detail.component.css']
+  selector: "app-house-detail",
+  templateUrl: "./house-detail.component.html",
+  styleUrls: ["./house-detail.component.css"]
 })
 export class HouseDetailComponent implements OnInit {
-
   card1: boolean;
   card2: boolean;
   card3: boolean;
   idImagen: number;
-
+  ArrayPhotos: Array<any>;
+  srcPrincipal: any;
 
   constructor() {
     this.card1 = true;
     this.card2 = false;
     this.card3 = false;
-
+    this.ArrayPhotos = [
+      {
+        id: 1,
+        src:
+          "https://www.kvik.es/~/media/images/bath/hero-baths/mano-bath/c1-1334x1000px-spot-campslidemob-textimgslidemob/mano-1330x1000px.jpg?bc=White&as=1&h=832&w=1110&useCustomFunctions=1&centerCrop=1&hash=44ACC81E4303C963C948FAD508D4A5006D66FB01"
+      },
+      {
+        id: 2,
+        src:
+          "https://www.tmagazine.es/app/uploads/2018/05/cocina_perfecta_03.jpg"
+      },
+      {
+        id: 3,
+        src:
+          "https://decoracion2.com/imagenes/2019/03/ideas-para-convertir-tu-terraza-en-un-auntentico-oasis-15.jpg"
+      },
+      {
+        id: 4,
+        src:
+          "https://odis.homeaway.com/odis/listing/a31268c2-cb75-4bd8-b039-4ff3cabcd66c.c10.jpg"
+      },
+      {
+        id: 5,
+        src:
+          "https://cdn.portobellostreet.es/imagenes_muebles/Muebles-Comedor-vintage-Maze.jpg"
+      },
+      {
+        id: 6,
+        src:
+          "https://www.amarehotels.com/wp-content/uploads/2019/07/01-amare-ibiza-habitaciones-oh-la-la-the-one-hab-1.jpg"
+      }
+    ];
   }
 
   ngOnInit() {
+    this.srcPrincipal = this.ArrayPhotos[0].src;
   }
 
-
-
-
+  changeImage(e) {
+    //console.log(e.toElement.src);
+    this.srcPrincipal = e.toElement.src;
+  }
 
   changeCard($event) {
-    console.log($event.target.id)
+    console.log($event.target.id);
     switch ($event.target.id) {
       case "1":
         this.card1 = true;
@@ -46,7 +79,5 @@ export class HouseDetailComponent implements OnInit {
         this.card3 = true;
         break;
     }
-
-  };
-
+  }
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Casa } from '../models/casas.model';
-import { HausesService } from '../hauses.service';
-import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Casa } from "../models/casas.model";
+import { HausesService } from "../hauses.service";
+import { Router } from "@angular/router";
 
 declare var google;
 
 @Component({
-  selector: 'app-search-hause',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search-hause",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"]
 })
-
 export class SearchComponent implements OnInit {
-
-  @ViewChild('googleMap', null)
+  @ViewChild("googleMap", null)
   gMap: any;
   map: any;
   directionsService: any;
@@ -21,129 +19,178 @@ export class SearchComponent implements OnInit {
   lat: any;
   lng: any;
   markers: any[] = [];
-
-  arrCasas: Casa[];
+  listHouses: any;
+  loading: boolean;
 
   constructor(private hausesService: HausesService, private router: Router) {
-    this.arrCasas = [
-      new Casa(1, '', '', '', '')
-    ]
-
-
+    this.listHouses = [
+      {
+        id: 1,
+        nombre: "La Casona",
+        direccion: "Llanes, Asturias, España ",
+        banos: 1,
+        camas: 5,
+        capacidad: 10,
+        foto:
+          "https://media.er2.co/es/avila/0000000014043/1600/52ef86c22befb.jpg",
+        descripcion: "asa en primera linea de playa."
+      },
+      {
+        id: 2,
+        nombre: "La recogida",
+        direccion: "Jaen, Jaen, España ",
+        banos: 1,
+        camas: 2,
+        capacidad: 2,
+        foto:
+          "https://www.planosdecasas3d.com/wp-content/uploads/2017/12/casa-economica-de-1-dormitorio-600x461.jpg",
+        descripcion: "Aire fresco y pura paz."
+      },
+      {
+        id: 3,
+        nombre: "La montañera",
+        direccion: "Andorra,Andorra, España ",
+        banos: 3,
+        camas: 6,
+        capacidad: 14,
+        foto:
+          "https://images.homify.com/c_fill,f_auto,h_500,q_auto,w_1280//p/photo/image/2734567/casa-prado-entre-bosques-2.jpg",
+        descripcion: "Disfruta de la nieve."
+      },
+      {
+        id: 4,
+        nombre: "El mar",
+        direccion: "La manga, Murcia, España ",
+        banos: 1,
+        camas: 1,
+        capacidad: 2,
+        foto:
+          "https://cflvdg.avoz.es/default/2019/06/30/00121561920814955999773/Foto/c.jpg",
+        descripcion: "Tu elijes donde bañarte."
+      },
+      {
+        id: 1,
+        nombre: "La Casona",
+        direccion: "Llanes, Asturias, España ",
+        banos: 1,
+        camas: 5,
+        capacidad: 10,
+        descripcion: "asa en primera linea de playa."
+      }
+    ];
   }
 
-  async ngOnInit() {
-    this.arrCasas = await this.hausesService.getAll();
-    console.log(this.arrCasas);
+  ngOnInit() {
+    this.loading = true;
 
     //Pide activar la geolocalizacion del navegador y pasa las coordenadas
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.showPosition.bind(this), this.showError)
+      navigator.geolocation.getCurrentPosition(
+        this.showPosition.bind(this),
+        this.showError
+      );
     } else {
-      console.log('La hemos liado con la geolocalizacion')
+      console.log("La hemos liado con la geolocalizacion");
     }
 
-    this.autocompletadoGoogle()
+    this.autocompletadoGoogle();
   }
 
   //Metodo que se ejecuta cuando estamos posicionados correctamente cuando se habilita la geolocalizacion
   showPosition(position) {
-    console.log(position)
-    this.loadMap(position)
+    console.log(position);
+    this.loadMap(position);
   }
 
   //Al cargar la pagina coge la geolocalizacion de showPosition y carga el mapa
   loadMap(position) {
-
-    this.directionsService = new google.maps.DirectionsService()
-    this.directionsDisplay = new google.maps.DirectionsRenderer()
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsDisplay = new google.maps.DirectionsRenderer();
 
     let propsMap = {
-      center: new google.maps.LatLng(position.coords.latitude,
-        position.coords.longitude),
+      center: new google.maps.LatLng(
+        position.coords.latitude,
+        position.coords.longitude
+      ),
       zoom: 17,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    this.map = new google.maps.Map(this.gMap.nativeElement, propsMap)
+    };
+    this.map = new google.maps.Map(this.gMap.nativeElement, propsMap);
 
-    this.directionsDisplay.setMap(this.map)
+    this.directionsDisplay.setMap(this.map);
 
     let marker = new google.maps.Marker({
       position: propsMap.center,
       map: this.map,
-      title: 'Usted se encuentra aqui!'
-    })
-    this.markers.push(marker)
+      title: "Usted se encuentra aqui!"
+    });
+    this.markers.push(marker);
+    this.loading = false;
   }
 
   // Carga el autompletado del input al iniciar la pagina.
   autocompletadoGoogle() {
+    let input = document.getElementById("inputPlace");
+    let autoComplete = new google.maps.places.Autocomplete(input);
 
-    let input = document.getElementById('inputPlace')
-    let autoComplete = new google.maps.places.Autocomplete(input)
+    autoComplete.setFields(["address_components", "geometry", "icon", "name"]);
 
-    autoComplete.setFields(['address_components', 'geometry', 'icon', 'name'])
-
-    autoComplete.addListener('place_changed', () => {
-      let place = autoComplete.getPlace()
-      let lat = place.geometry.location.lat()
-      let lng = place.geometry.location.lng()
+    autoComplete.addListener("place_changed", () => {
+      let place = autoComplete.getPlace();
+      let lat = place.geometry.location.lat();
+      let lng = place.geometry.location.lng();
       // console.log(lat)
       // console.log(lng)
       let m = new google.maps.Marker({
         animation: google.maps.Animation.DROP
-      })
-      this.lat = lat
-      this.lng = lng
-      console.log(this.lat, this.lng)
-    })
+      });
+      this.lat = lat;
+      this.lng = lng;
+      console.log(this.lat, this.lng);
+    });
   }
 
   // Cuando clicamos el boton carga el mapa con la direccion que hayamos puesto en el input
   mapa() {
-
-    this.directionsService = new google.maps.DirectionsService()
-    this.directionsDisplay = new google.maps.DirectionsRenderer()
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsDisplay = new google.maps.DirectionsRenderer();
 
     let propsMap = {
-      center: new google.maps.LatLng(this.lat,
-        this.lng),
+      center: new google.maps.LatLng(this.lat, this.lng),
       zoom: 17,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    this.map = new google.maps.Map(this.gMap.nativeElement, propsMap)
+    };
+    this.map = new google.maps.Map(this.gMap.nativeElement, propsMap);
 
-    this.directionsDisplay.setMap(this.map)
+    this.directionsDisplay.setMap(this.map);
 
     let m = new google.maps.Marker({
       position: new google.maps.LatLng(this.lat, this.lng),
       map: this.map,
-      title: 'Nuevo punto'
+      title: "Nuevo punto"
     });
-    this.markers.push(m)
+    this.markers.push(m);
   }
 
   // Controla los errores de geolocalizacion
   showError(error) {
-    console.log(error)
+    console.log(error);
     switch (error.code) {
-
       case error.PERMISSION_DENIED:
-        console.log('El usuario no quiere ser localizado')
-        break
+        console.log("El usuario no quiere ser localizado");
+        break;
 
       case error.POSITION_UNAVAILABLE:
-        console.log('No se ha podido recuperar la posición')
-        break
+        console.log("No se ha podido recuperar la posición");
+        break;
 
       case error.TIMEOUT:
-        console.log('Se ha tardado demasiado en recuperar la localización')
-        break
+        console.log("Se ha tardado demasiado en recuperar la localización");
+        break;
 
       case error.UNKNOWN_ERROR:
-        console.log('Error desconocido')
-        break
+        console.log("Error desconocido");
+        break;
     }
   }
-
 }
