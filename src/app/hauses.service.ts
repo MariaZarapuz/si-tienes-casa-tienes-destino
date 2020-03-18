1b73c20c0e208909e369397ae67a05e01a932cfb
katekoleva <kolevadk95@gmail.com>
2020-03-18 10:17:48 +0100

    eliminacion de  errores 10:17 18/03/2020

--- src/app/hauses.service.ts
+++ src/app/hauses.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@angular/core";
-import { Casa } from "../app/models/casas.model";
-import { HttpClient } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { Casa } from '../app/models/casas.model';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class HausesService {
   baseUrl: string;
   constructor(private httpClient: HttpClient) {
-    this.baseUrl = "http://localhost:3000/api/";
+    this.baseUrl = 'http://localhost:3000/api/';
   }
 }
