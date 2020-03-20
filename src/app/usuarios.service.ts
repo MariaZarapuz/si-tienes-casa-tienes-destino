59005567eda2032aa0577eff85ac90a75916a542
katekoleva <kolevadk95@gmail.com>
2020-03-20 13:56:34 +0100

    cambios 20/03/2020

---
+++ src/app/usuarios.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+
+   baseUrl: string;
+
+  constructor( private httpClient: HttpClient) {
+    this.baseUrl = 'http://localhost:3000/api/users';
+   }
+
+   createUsuario(formValue): Promise<any> {
+     return this.httpClient.post (this.baseUrl, formValue).toPromise();
+   }
+}
