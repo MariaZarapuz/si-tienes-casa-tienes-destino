1384cf27391acc4e20a76266c2178157b6e65bc4
MariaZarapuz <mariazarapuz@gmail.com>
2020-03-23 17:30:55 +0100

    login terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
+import { UsuariosService } from "./usuarios.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'Front';
-  constructor() {}
+export class AppComponent implements OnInit {
+  title = "Front";
+  show: string;
+  constructor(private usuariosService: UsuariosService) {}
+  ngOnInit() {
+    this.show = this.usuariosService.getLocalStore("token");
+    console.log(this.show);
+  }
 }
